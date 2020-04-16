339a1bac3446b5b9a76a77f24a2290aecf1c9917
Gabriel Peres Magni <gpmagni@gmail.com>
2020-04-16 20:10:38 -0300

    Delete delay

--- web/script.php
+++ web/script.php
@@ -11,34 +11,10 @@ if ((isset($_POST['ping']) && isset($_POST['download']) && isset($_POST['upload'
 
     $arquivo = $pwd . "/arquivo/arquivo.log";
     file_put_contents($arquivo, $dados, FILE_APPEND);
+    echo "<script>window.close();</script>";
 
-    $dados[0] = "Ping: " . $_POST['ping'] . "ms <br>";
-    $dados[1] = "Download: " . $_POST['download'] . "Mbps <br>";
-    $dados[2] = "Upload: " . $_POST['upload'] . "Mbps <br>";
+
 ?>
-
-    <html>
-
-    <head>
-        <title>Página WEB OK!</title>
-    </head>
-
-    <body>
-        <h1><?php
-            try {
-                for ($i = 0; $i < 3; $i++) {
-                    echo $dados[$i];
-                }
-            } catch (\Throwable $th) {
-                echo "Nada para ver aqui, siga em frente";
-            } finally {
-                sleep(5);
-                echo "<script>window.close();</script>";
-            }
-            ?></h1>
-    </body>
-
-    </html>
 
 
 <?php
